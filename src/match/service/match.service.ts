9014b6a6d352a1ffc1e9ac746fc76c5157315b36
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2025-04-16 01:59:01 -0300

    atualiza o serviço de partidas para processar e formatar eventos retornados pelo provedor de dados

--- src/match/service/match.service.ts
+++ src/match/service/match.service.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from "@nestjs/common";
 import { DataProviderInterface } from "src/providers/interfaces/data-providers.interface";
+import { EventList } from "src/providers/sofascore/interfaces/events.interface";
 
 @Injectable()
 export class MatchService {
@@ -9,6 +10,17 @@ export class MatchService {
     ) {}
 
     async getMatch(day: string): Promise<any> {
-        return await this.dataProvider.getEvents(day);
+        const events: EventList = await this.dataProvider.getEvents(day);
+        const matches = events.events.map(event => {
+            return {
+                id: event.id,
+                date: new Date(event.startTimestamp * 1000),
+                homeTeam: event.homeTeam.name,
+                awayTeam: event.awayTeam.name,
+                tournament: event.tournament.name,
+                status: event.status,
+            };
+        });
+        return matches;
     }
 }

0d0e37b58eadfe4aa8058cacb7edcc8e63cb3530
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2025-04-20 14:24:26 -0300

    filtra eventos com status específico antes de mapear para o formato de partidas

--- src/match/service/match.service.ts
+++ src/match/service/match.service.ts
@@ -11,16 +11,18 @@ export class MatchService {
 
     async getMatch(day: string): Promise<any> {
         const events: EventList = await this.dataProvider.getEvents(day);
-        const matches = events.events.map(event => {
-            return {
-                id: event.id,
-                date: new Date(event.startTimestamp * 1000),
-                homeTeam: event.homeTeam.name,
-                awayTeam: event.awayTeam.name,
-                tournament: event.tournament.name,
-                status: event.status,
-            };
-        });
+        const matches = events.events
+            .filter(event => event.status.code !== 100)
+            .map(event => {
+                return {
+                    id: event.id,
+                    date: new Date(event.startTimestamp * 1000),
+                    homeTeam: event.homeTeam.name,
+                    awayTeam: event.awayTeam.name,
+                    tournament: event.tournament.name,
+                    status: event.status,
+                };
+            });
         return matches;
     }
 }

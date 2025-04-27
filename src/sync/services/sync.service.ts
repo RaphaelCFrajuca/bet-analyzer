942fbe390da0ae6d958e21320e78dae00adeef59
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2025-04-27 05:48:57 -0300

    fix: corrige a data de sincronização para incluir o dia seguinte

--- src/sync/services/sync.service.ts
+++ src/sync/services/sync.service.ts
@@ -25,8 +25,9 @@ export class SyncService {
     }
 
     async syncAllData(): Promise<void> {
-        // new Date(Date.now() + 24 * 60 * 60 * 1000)
-        const date = utcToZonedTime(new Date(), "America/Sao_Paulo").toISOString().split("T")[0];
+        const date = utcToZonedTime(new Date(Date.now() + 24 * 60 * 60 * 1000), "America/Sao_Paulo")
+            .toISOString()
+            .split("T")[0];
         console.log("Syncing all matches for day ", date);
         const matches: Match[] = await this.matchService.getMatch(date, true);
 

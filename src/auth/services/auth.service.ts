48fa0844b8df904f456111778de208b921d66490
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2025-04-28 21:42:00 -0300

    feat: adiciona suporte a autenticação com bcrypt e melhorias no AuthService

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,7 +1,45 @@
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, NotFoundException, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import * as bcrypt from "bcrypt";
 import { Database } from "src/database/providers/interfaces/database.interface";
 
 @Injectable()
 export class AuthService {
-    constructor(@Inject("DATABASE_PROVIDER") private readonly databaseProvider: Database) {}
+    constructor(
+        @Inject("DATABASE_PROVIDER") private readonly databaseProvider: Database,
+        @Inject("JWT_TOKEN_SECRET") private readonly jwtTokenSecret: string,
+        private readonly jwtService: JwtService,
+    ) {}
+
+    async login(username: string, password: string): Promise<AuthResponse> {
+        const user = await this.databaseProvider.findByUsername(username);
+        if (!user) {
+            throw new NotFoundException("User not found");
+        }
+        if (user.password !== password) {
+            throw new UnauthorizedException("Invalid password");
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            throw new UnauthorizedException("Invalid password");
+        }
+
+        const jwtToken = await this.jwtService.signAsync(
+            {
+                id: user.id,
+                username: user.username,
+            },
+            {
+                secret: this.jwtTokenSecret,
+            },
+        );
+        return {
+            token: jwtToken,
+        };
+    }
+}
+
+export interface AuthResponse {
+    token: string;
 }

e2e91590c2435cc7ff9aecbbaaf2e4972d84e0ed
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2025-04-28 21:19:56 -0300

    feat: atualiza a função databaseFactory para usar enum DatabaseProvider

--- src/database/database.factory.ts
+++ src/database/database.factory.ts
@@ -2,11 +2,15 @@ import { Database } from "./providers/interfaces/database.interface";
 import { PostgresqlConfig } from "./providers/postgresql/interfaces/postgresql-config.interface";
 import { PostgresqlProvider } from "./providers/postgresql/postgresql.provider";
 
-export function databaseFactory(database: string, config: PostgresqlConfig): Database {
+export function databaseFactory(database: DatabaseProvider, config: PostgresqlConfig): Database {
     switch (database) {
-        case "postgresql":
+        case DatabaseProvider.POSTGRESQL:
             return new PostgresqlProvider(config);
         default:
-            throw new Error(`Database ${database} not supported`);
+            throw new Error(`Database ${String(database)} not supported`);
     }
+}
+
+enum DatabaseProvider {
+    POSTGRESQL = "postgresql",
 }

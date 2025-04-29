69f204e579693fb23dd82e38c115f6e4974ba723
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2025-04-28 21:22:23 -0300

    feat: adiciona DatabaseModule às importações do AuthModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { DatabaseModule } from "src/database/database.module";
 import { EnvironmentModule } from "src/environment/environment.module";
 import { AuthGuard } from "./guards/auth.guard";
 
 @Module({
     imports: [
         EnvironmentModule,
+        DatabaseModule,
         JwtModule.register({
             signOptions: {
                 expiresIn: "1d",
